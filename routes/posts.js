6405d73309137466a064b9aeef7bac4f4520688d
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2022-09-05 23:49:52 +0300

    Add files via upload

---
+++ routes/posts.js
@@ -0,0 +1,146 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+//create a post
+
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//update a post
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//delete a post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//like / dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    let statusType = false;
+    let val = {
+     userId:req.body.userId,
+     typeLike: req.body.typeLike?req.body.typeLike:"like"
+    }
+     
+    await post.likes.forEach(like => {
+     if(like.userId == req.body.userId ){
+        statusType = true;
+     }
+    });
+    if (!statusType) {
+      await post.updateOne({ $push: { likes: val } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: val } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ });
+/*router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});*/
+//get a post
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get timeline posts
+
+router.get("/timeline/:userId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
+
+//get user's all posts
+
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    await console.log(user)
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.put("/commit/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  let val={
+    commit: req.body.commit,
+    userName:req.body.userName
+  }
+  try {
+   await post.updateOne({ $push: { commit: val } });
+    res.status(200).json("add commit");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
+router.get('/commentsAll/:id',async (req,res)=>{
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post.commit);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+  
+  
+})
